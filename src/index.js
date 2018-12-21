1969a8751647069c4692acd3970e56bfcac20afd
yuchiu <yuchiu2002@hotmail.com>
2018-12-21 00:23:30 -0500

    updated with new versions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../assets/scss/main.scss";
-import Home from "./components/Home";
+import dotenv from "dotenv";
 
-const app = <Home />;
+import Routes from "./components";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(app, document.getElementById("root"));
+dotenv.config();
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
